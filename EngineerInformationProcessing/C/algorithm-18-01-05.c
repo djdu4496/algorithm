fa4029480626741ba8f47f6306fb9b9d0467d128
djdu4496 <djdu4496@gmail.com>
2022-04-11 15:41:58 +0900

    feat : 정.처.기 실기 2018년도 1회

---
+++ EngineerInformationProcessing/C/algorithm-18-01-05.c
@@ -0,0 +1,17 @@
+// 2018년 1회 기사 실기 5번
+
+#include <stdio.h>
+
+main() {
+	int input, sum = 0;
+	scanf("%d", &input);
+	while (1) {
+		if (( input ) == 0)
+			break;
+		sum = sum + input % 10;
+		input = input / ( 10 );
+	}
+	printf("%d\n", sum);
+}
+
+// 블로그 링크: https://cooing-silicon-7ae.notion.site/04-C-7dcc117c3c414d6cabf0bce9bce878ae
---
+++ EngineerInformationProcessing/Java/algorithm-18-01-06.java
@@ -0,0 +1,31 @@
+public class Test1 {
+	public static void main(String[] args) {
+		int E[] = { 95, 75, 85, 100, 50};
+		int i = 0;
+		int Temp = 0;
+
+		do
+		{
+			int j = i;
+			do 
+			{
+				if( E[i] > E[j] )
+				{
+					Temp = E[i];
+					E[i] = E[j];
+					E[j] = Temp;
+				}
+				j++;
+			} while (j < 5);
+			i++;
+		} while (i < 4);
+	
+	for (int a =0; a < 5; a++)
+	{
+		System.out.printf(E[a]+"\t");
+	}
+	System.out.println();
+	}
+}
+
+// 블로그 링크: https://cooing-silicon-7ae.notion.site/01-JAVA-6-039add39cbc942d79f467e9651b3589e
