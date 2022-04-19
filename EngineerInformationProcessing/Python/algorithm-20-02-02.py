4e6ba94244f987684bf960a316053cba342f42be
djdu4496 <djdu4496@gmail.com>
2022-04-20 02:07:50 +0900

    feat : 정.처.기 실기 2020년도 2회 2번 파이썬

---
+++ EngineerInformationProcessing/Python/algorithm-20-02-02.py
@@ -0,0 +1,10 @@
+# 2020년 2회 기사 실기 2번
+
+asia={'일본',	'중국',	'한국'}
+asia.add('베트남')
+asia.add('중국')
+asia.remove('일본')
+asia.update({'홍콩','한국','태국'})
+print(asia) 
+
+# 블로그 링크: https://cooing-silicon-7ae.notion.site/02-2-cc3c7aaaba3846f89dc80e2770168309
--- EngineerInformationProcessing/Python/algorithm-21-03-14.py
+++ EngineerInformationProcessing/Python/algorithm-21-03-14.py
@@ -1,5 +1,7 @@
+# 2021년 3회 기사 실기 14번
+
 a, b = 100, 200
 print(a==b)
 
-// 할당된 값들이 서로 같은지 boolean 값을 출력하면 된다.
-// boolean값은 True, False 처럼 대문자로 시작한다.
+# 할당된 값들이 서로 같은지 boolean 값을 출력하면 된다.
+# boolean값은 True, False 처럼 대문자로 시작한다.
