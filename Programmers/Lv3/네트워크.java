d9ad4a54d5f41625c112848c8316e2394ccbf364
unknown <djdu4496@github.com>
2025-06-23 23:16:24 +0900

    깊이/너비 우선 탐색(DFS/BFS) > 네트워크"

---
+++ Programmers/Lv3/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    // DSF 
+    // 1. 탐색 시작 노드를 스택 삽입하고 방문처리한다.
+    // 2. 스택 최상위 노드에 방문한 적 없는 인접한 노드가 하나라도 있으면 그 노드를 스택에 삽입하고 방문처리한다.
+    //    방문하지 않은 인접 노드가 없으면 스택에서 최상위 노드를 꺼낸다.
+    // 3. 2번의 과정을 더 이상 수행할 수 없을 때 까지 반복한다.
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] isVisited = new boolean[n];
+        for(int i=0; i<n; i++){
+                if(!isVisited[i]){
+                     dfs(computers, i, isVisited);
+                    answer++;
+                }
+        }
+        return answer;
+    }
+    private void dfs(int[][] computers, int i, boolean[] isVisited) {
+        isVisited[i] = true;
+        for(int j=0; j < computers[i].length; j++){
+    if(i != j && computers[i][j] == 1 && !isVisited[j])
+       dfs(computers, j, isVisited);
+}
+    }
+} 
